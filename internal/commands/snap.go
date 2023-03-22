cebac1ab4c94ced99ade95edda594fd982208555
Robert P. J. Day <robert.day@canonical.com>
2023-03-22 10:46:04 -0400

    Fix typo in help string: "Pressed" -> "Preseed"
    Signed-off-by: Robert P. J. Day <robert.day@canonical.com>

--- internal/commands/snap.go
+++ internal/commands/snap.go
@@ -9,7 +9,7 @@ type SnapArgs struct {
 type SnapOpts struct {
 	DisableConsoleConf bool `long:"disable-console-conf" description:"Disable console-conf on the resulting image."`
 	FactoryImage       bool `long:"factory-image" description:"Hint that the image is meant to boot in a device factory."`
-	Preseed            bool `long:"preseed" description:"Pressed the image (UC20 only)."`
+	Preseed            bool `long:"preseed" description:"Preseed the image (UC20 only)."`
 
 	AppArmorKernelFeaturesDir string `long:"apparmor-features-dir" description:"Optional path to apparmor kernel features directory"`
 	PreseedSignKey            string `long:"preseed-sign-key" description:"Name of the key to use to sign preseed assertion, otherwise use the default key"`
