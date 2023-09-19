7d7e88577cacc8623533ac6c610256e9e0842088
Paul Mars <paul.mars@canonical.com>
2023-09-19 17:55:24 +0200

    Mark asserter methods as test helpers
    This object should be moved to a dedicated pacakge to avoid mixing actual code helpers and test helpers. This would make a clear distinctions between those two kind and avoid using test helpers in actual code by mistake.
    It would also avoid reporting coverage on this code.

--- internal/helper/asserter.go
+++ internal/helper/asserter.go
@@ -17,6 +17,7 @@ type Asserter struct {
 // ends the test on a non-nil error. Otherwise, it lets the test continue running
 // but marks it as failed
 func (asserter *Asserter) AssertErrNil(err error, fatal bool) {
+	asserter.Helper()
 	if err != nil {
 		debug.PrintStack()
 		if fatal {
@@ -29,6 +30,7 @@ func (asserter *Asserter) AssertErrNil(err error, fatal bool) {
 // AssertErrContains asserts that an error is non-nil, and that the error
 // message string contains a sub-string that is passed in
 func (asserter *Asserter) AssertErrContains(err error, errString string) {
+	asserter.Helper()
 	if err == nil {
 		debug.PrintStack()
 		asserter.Error("Expected an error, but got none")
@@ -42,6 +44,7 @@ func (asserter *Asserter) AssertErrContains(err error, errString string) {
 
 // AssertEqual asserts that two objects are equal using go-cmp
 func (asserter *Asserter) AssertEqual(want, got interface{}, cmpOpts ...cmp.Option) {
+	asserter.Helper()
 	diff := cmp.Diff(want, got, cmpOpts...)
 	if want != nil && diff != "" {
 		asserter.Errorf("mismatch (-want +got):\n%s", diff)
