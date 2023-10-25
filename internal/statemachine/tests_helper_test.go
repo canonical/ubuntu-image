8c3fc9b3635c4c913c10b28c84327c2b55cadaf1
Paul Mars <paul.mars@canonical.com>
2023-10-25 18:01:39 +0200

    Add a basicChrooter to generic a common basic chroot

--- internal/statemachine/tests_helper_test.go
+++ internal/statemachine/tests_helper_test.go
@@ -5,7 +5,92 @@ import (
 	"io/fs"
 	"os"
 	"os/exec"
+	"sync"
+
+	"github.com/snapcore/snapd/osutil"
+
+	"github.com/canonical/ubuntu-image/internal/helper"
+	"github.com/canonical/ubuntu-image/internal/imagedefinition"
 )
+
+func init() {
+	basicChroot = NewBasicChroot()
+}
+
+var basicImageDef = imagedefinition.ImageDefinition{
+	Architecture: getHostArch(),
+	Series:       getHostSuite(),
+	Rootfs: &imagedefinition.Rootfs{
+		Archive: "ubuntu",
+	},
+	Customization: &imagedefinition.Customization{},
+}
+
+// basicChroot holds the path to the basic chroot
+// this variable should be treated as a singleton
+var basicChroot *basicChrooter
+
+type basicChrooter struct {
+	m    sync.Mutex
+	path string
+}
+
+func NewBasicChroot() *basicChrooter {
+	return &basicChrooter{
+		m: sync.Mutex{},
+	}
+}
+
+func (b *basicChrooter) isInit() bool {
+	b.m.Lock()
+	defer b.m.Unlock()
+	return len(b.path) != 0
+}
+
+func (b *basicChrooter) init() error {
+	// We need to protect the whole time the chroot is being created
+	// because we do not want to trigger multiple chroot creation in parallel
+	b.m.Lock()
+	defer b.m.Unlock()
+
+	var stateMachine ClassicStateMachine
+	stateMachine.commonFlags, stateMachine.stateMachineFlags = helper.InitCommonOpts()
+	stateMachine.parent = &stateMachine
+	stateMachine.ImageDef = basicImageDef
+	// prepare the dir
+
+	err := stateMachine.createChroot()
+	if err != nil {
+		return err
+	}
+
+	basicChroot.path = stateMachine.tempDirs.chroot
+
+	return nil
+}
+
+// getBasicChroot initializes and/or set the basicChroot path to
+// the given stateMachine
+func getBasicChroot(s StateMachine) error {
+	if basicChroot.isInit() {
+
+		return setBasicChrootPath(s)
+	}
+
+	err := basicChroot.init()
+	if err != nil {
+		return err
+	}
+
+	return setBasicChrootPath(s)
+}
+
+func setBasicChrootPath(s StateMachine) error {
+	basicChroot.m.Lock()
+	defer basicChroot.m.Unlock()
+
+	return osutil.CopySpecialFile(basicChroot.path, s.tempDirs.chroot)
+}
 
 type osMockConf struct {
 	osutilCopySpecialFileThreshold uint
