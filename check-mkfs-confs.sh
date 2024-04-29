9128b215396cbf1fd111e29839fc11712fd32b86
Paul Mars <paul.mars@canonical.com>
2024-04-29 14:38:35 +0200

    Fix mkfs db comparison
    Signed-off-by: Paul Mars <paul.mars@canonical.com>

--- check-mkfs-confs.sh
+++ check-mkfs-confs.sh
@@ -9,7 +9,9 @@ DB="$WORKDIR"/mkfs/db
 NEW_DB="$WORKDIR"/mkfs/new_db
 MUST_UPDATE=0
 
-rm "$NEW_DB" || true
+if [ -d "$NEW_DB" ]; then
+    rm "$NEW_DB" 
+fi
 
 IFS=$'\n'
 
@@ -22,20 +24,20 @@ TOTAL_SUPPORTED_SERIES=$(sort supported supported_esm | uniq)
 
 rm supported supported_esm
 
-
 for FULL_SERIES in $TOTAL_SUPPORTED_SERIES; do
     SERIES=$(echo "$FULL_SERIES" | awk '{split($0,r,"\""); print tolower(r[2])}' | cut -d " " -f 1)
     NEW_VERSION=$(echo "$CANDIDATES" | grep "$SERIES" | tail -1 | cut -d "," -f 2)
     echo "$SERIES,$NEW_VERSION" >> "$NEW_DB"
 done
 
+count_old=$(wc -l < "$DB")
+count_new=$(wc -l < "$NEW_DB")
 
-if [ "$(wc -l < "$DB" )" != "$(wc -l < "$NEW_DB")" ]; then
-    echo "mkfs configurations different. Please run collect-mkfs-confs.sh and commit the resulting configuration."
+if [ "$count_old" != "$count_new" ]; then
+    echo "mkfs configurations different. Please run collect-mkfs-confs.sh and commit the resulting configuration. Found $count_new but got $count_old"
     MUST_UPDATE=1
     exit $MUST_UPDATE
 fi
-
 
 IFS=$'\n'
 while read -r entry; do
